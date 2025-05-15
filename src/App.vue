13cf35e1aecbe19f0fd620eea82bee3fed1b322c
vuelta7 <markvuelta07@example.com>
2025-05-15 16:47:05 +0800

    failed calculator

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="flex justify-center items-center h-screen">
-    <Counter />
-    <Fetchfile />
-  </div>
+  <Calculator />
 </template>
 
 <script setup>
-import Counter from "./components/Counter.vue";
-import Fetchfile from "./components/FetchFile.vue";
+import Calculator from "./components/Calculator.vue";
 </script>
